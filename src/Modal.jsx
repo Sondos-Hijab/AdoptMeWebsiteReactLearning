e214c437f030d64c8326ef9b109ffd4acf0d09de
Sondos Hijab <sondos.hijab@gmail.com>
2023-01-16 03:02:26 +0200

    editing Modal.jsx

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,16 +1,14 @@
 import React, { useEffect, useRef } from "react";
-
 import { createPortal } from "react-dom";
 
 const Modal = ({ children }) => {
   const elRef = useRef(null);
-
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
 
   useEffect(() => {
-    const modalRoot = document.getElementById("Modal");
+    const modalRoot = document.getElementById("modal");
     modalRoot.appendChild(elRef.current);
     return () => modalRoot.removeChild(elRef.current);
   }, []);
